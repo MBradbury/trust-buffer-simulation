7de25202158701bc49ea2af542c049372224ce41
Matthew Bradbury <matt-bradbury@live.co.uk>
2020-11-24 10:28:21 +0000

    Run simulations with 2 always bad agents

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ ESs="Random FIFO LRU MRU Chen2016 FiveBand"
 rm -f *.pdf
 
 SEED=2
-NUM_AGENTS=10
+NUM_AGENTS=8
+NUM_BAD_AGENTS=2
 NUM_CAPABILITIES=2
 DURATION=120
 AGENT_CHOOSE=Random
@@ -26,7 +27,8 @@ do
 	do
 		echo "Running $ES"
 
-		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --num-capabilities $NUM_CAPABILITIES --duration $DURATION \
+		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --agents $NUM_BAD_AGENTS AlwaysBadBehaviour \
+			--num-capabilities $NUM_CAPABILITIES --duration $DURATION \
 			--max-crypto-buf 10 --max-trust-buf 20 --max-reputation-buf 10 --max-stereotype-buf 20 \
 			--eviction-strategy "$ES" --agent-choose "$AGENT_CHOOSE" --utility-targets "$UTILITY_TARGETS" \
 			--seed $SEED > "log-$ES.txt"
@@ -41,7 +43,8 @@ do
 	do
 		echo "Running $ES"
 
-		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --num-capabilities $NUM_CAPABILITIES --duration $DURATION \
+		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --agents $NUM_BAD_AGENTS AlwaysBadBehaviour \
+			--num-capabilities $NUM_CAPABILITIES --duration $DURATION \
 			--max-crypto-buf 10 --max-trust-buf 10 --max-reputation-buf 10 --max-stereotype-buf 10 \
 			--eviction-strategy "$ES" --agent-choose "$AGENT_CHOOSE" --utility-targets "$UTILITY_TARGETS" \
 			--seed $SEED > "log-$ES.txt"
@@ -56,7 +59,8 @@ do
 	do
 		echo "Running $ES"
 
-		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --num-capabilities $NUM_CAPABILITIES --duration $DURATION \
+		./run_simulation.py --agents $NUM_AGENTS $BEHAVIOUR --agents $NUM_BAD_AGENTS AlwaysBadBehaviour \
+			--num-capabilities $NUM_CAPABILITIES --duration $DURATION \
 			--max-crypto-buf 5 --max-trust-buf 5 --max-reputation-buf 5 --max-stereotype-buf 5 \
 			--eviction-strategy "$ES" --agent-choose "$AGENT_CHOOSE" --utility-targets "$UTILITY_TARGETS" \
 			--seed $SEED > log-$ES.txt
