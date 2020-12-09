b448d922653a87eb82ff50a4ca6d66113dc88f80
Matthew Bradbury <matt-bradbury@live.co.uk>
2020-12-09 17:15:00 +0000

    Add a script to graph space consumption of trust models

---
+++ graph_space.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import pickle
+import subprocess
+import itertools
+from itertools import chain
+from multiprocessing import Pool
+import functools
+from pprint import pprint
+
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from simulation.metrics import Metrics
+from simulation.capability import CapabilityBehaviourState, InteractionObservation
+
+from analysis import savefig
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.size'] = 12
+
+def graph_space(path_prefix: str):
+    fig = plt.figure()
+    ax = fig.gca()
+
+    def fn(v, c, t):
+        crypto = 160 * v
+        trust = t * v + t * v * c
+        reputation = v * trust
+        stereotype = t * v * c
+
+        return crypto + trust + reputation + stereotype
+
+    vcs = [
+        (10, 1),
+        (10, 2),
+        (20, 1),
+    ]
+
+    Xs = np.arange(1, 44, 1)
+
+    for (v, c) in vcs:
+        Ys = np.vectorize(functools.partial(fn, v, c))(Xs)
+
+        ax.plot(Xs, Ys, label=f"$|V|={v}$ $|C|={c}$")
+
+    ax.axhline(y=32*1024, label="Zolertia RE Mote total memory", color="red")
+
+    ax.axvline(x=8, label="$T$ (BRS)", linestyle="--", color="tab:purple")
+    ax.axvline(x=40, label="$T$ (HMM with 2 states, 2 observations)", linestyle="--", color="tab:olive")
+
+    ax.set_xlabel('Trust model size (bytes)')
+    ax.set_ylabel('Total size (bytes)')
+
+    ax.legend(bbox_to_anchor=(0.45, 1.3), loc="upper center", ncol=2)
+
+    savefig(fig, f"{path_prefix}space.pdf")
+
+    plt.close(fig)
+
+def main(args):
+    graph_space(args.path_prefix)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Analyse')
+    parser.add_argument('--path-prefix', type=str, default="",
+                        help='The prefix to the location to output results')
+
+    args = parser.parse_args()
+
+    main(args)
