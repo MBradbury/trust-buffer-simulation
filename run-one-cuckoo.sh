37b29355fd2d516a1938673f87b906ec4fdcb113
Matthew Bradbury <m.s.bradbury@lancaster.ac.uk>
2024-10-28 08:58:57 +0000

    Update run-one-cuckoo.sh to use teh Cuckoo eviction strategy

--- run-one-cuckoo.sh
+++ run-one-cuckoo.sh
@@ -5,7 +5,7 @@ NUM_BEHAVIOUR_AGENTS=2
 NUM_BAD_AGENTS=8
 NUM_CAPABILITIES=2
 DURATION=300
-ES="LRU"
+ES="Cuckoo"
 AGENT_CHOOSE="Cuckoo"
 UTILITY_TARGETS="good"
 
@@ -18,4 +18,6 @@ python -OO run_simulation.py --agents $NUM_BEHAVIOUR_AGENTS $BEHAVIOUR --agents 
             --challenge-execution-time 5 \
 			--max-crypto-buf 10 --max-trust-buf 20 --max-reputation-buf 10 --max-stereotype-buf 20 --max-cr-buf 10 --cuckoo-max-capacity 20 \
 			--eviction-strategy "$ES" --agent-choose "$AGENT_CHOOSE" --utility-targets "$UTILITY_TARGETS" \
-			--seed $SEED --path-prefix "results/cuckoo/$BEHAVIOUR/$ES/complete-" --log-level 0
+			--seed $SEED --path-prefix "results/$AGENT_CHOOSE/$BEHAVIOUR/$ES/complete-" --log-level 0
+
+python graph_individual.py results/$AGENT_CHOOSE/$BEHAVIOUR/$ES/complete-metrics.$SEED.pickle.bz2
